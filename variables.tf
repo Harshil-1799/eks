5837ed43eeb8c1a3cbc19ea37cd802a0c7255ba3
Harshil-1799 <shahharshil1706@gmail.com>
2023-08-10 11:26:10 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "access_key"{
+ default ="AKIAZLRD5VYRCILRTEHK"  
+}
+variable "secret_key"{
+ default ="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"  
+}
